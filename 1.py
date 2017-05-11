bd7e56ba514013f5ffc576d277f1551d09513c6a
ardany26 <mmmardny@gmail.com>
2017-05-10 22:58:23 -0500

    versión 2
    quitamos la imagen de prueba y ahora sí, se coloca la imagen que va a ir
    en el juego

--- 1.py
+++ 1.py
@@ -21,6 +21,58 @@ def Jugar ():
         nombres.geometry("700x400")
 
 
+
+        def Nivel1():
+
+                nombres.destroy()
+                Nivel1 = Tk()
+                Nivel1.title("ROAD FIGHTER")
+                Nivel1.geometry("1450x800")
+
+                imagen2 = PhotoImage(file="carretera_principal.PNG")
+                fondo2 = Label(Nivel1,image=imagen2).place(x=0,y=0)
+
+                jugador1 = ttk.Label(Nivel1,text=usuario1).place(x=400,y=200)
+                
+                jugador2 = ttk.Label(Nivel1,text=usuario2).place(x=400,y=500)
+               
+                auto1 = PhotoImage(file="coche-jugador1.PNG")
+                etiq_auto = Label(Nivel1,image=auto1).place(x=300,y=590)
+
+                usuario1.get()
+                usuario2.get()
+                Nivel1.mainloop()
+                
+
+        def Nivel2():
+
+                nombres.destroy()
+                Nivel2 = Tk()
+                Nivel2.title("ROAD FIGHTER")
+                Nivel2.geometry("1450x800")
+                
+
+        def Nivel3():
+
+                nombres.destroy()
+                Nivel3 = Tk()
+                Nivel3.title("ROAD FIGHTER")
+                Nivel3.geometry("1450x800")
+
+        def Nivel4():
+
+                nombres.destroy()
+                Nivel4 = Tk()
+                Nivel4.title("ROAD FIGHTER")
+                Nivel4.geometry("1450x800")
+
+        def Nivel5():
+
+                nombres.destroy()
+                Nivel5 = Tk()
+                Nivel5.title("ROAD FIGHTER")
+                Nivel5.geometry("1450x800")
+
         etiqueta1 = Label(nombres,text="Jugador 1:").place(x=100,y=70)
                            
         usuario1 = StringVar()
@@ -32,52 +84,6 @@ def Jugar ():
         usuario2 = StringVar()
 
         ctx2 = Entry(nombres,textvariable=usuario2, width=50).place(x=100,y=220)
-
-        def Nivel1():
-
-                nombres.destroy()
-                Nivel1 = Tk()
-                Nivel1.title("NIVEL 1")
-                Nivel1.geometry("1450x800")
-
-                imagen2 = PhotoImage(file="carretera2.PNG")
-                fondo2 = Label(Nivel1,image=imagen2).pack(padx=0,pady=0)
-                
-                auto1 = PhotoImage(file="coche-jugador1.PNG")
-                etiq_auto = Label(Nivel1,image=auto1).place(x=300,y=590)
-                
-
-                Nivel1.mainloop()
-                
-
-        def Nivel2():
-
-                nombres.destroy()
-                Nivel2 = Tk()
-                Nivel2.title("NIVEL 2")
-                Nivel2.geometry("1450x800")
-                
-
-        def Nivel3():
-
-                nombres.destroy()
-                Nivel3 = Tk()
-                Nivel3.title("NIVEL 3")
-                Nivel3.geometry("1450x800")
-
-        def Nivel4():
-
-                nombres.destroy()
-                Nivel4 = Tk()
-                Nivel4.title("NIVEL 4")
-                Nivel4.geometry("1450x800")
-
-        def Nivel5():
-
-                nombres.destroy()
-                Nivel5 = Tk()
-                Nivel5.title("NIVEL 5")
-                Nivel5.geometry("1450x800")
         
         nivel1 = Button(nombres,text="NIVEL 1",command=Nivel1,height=3,width=7).place(x=100,y=300)
         nivel2 = Button(nombres,text="NIVEL 2",command=Nivel2,height=3,width=7).place(x=200,y=300)
